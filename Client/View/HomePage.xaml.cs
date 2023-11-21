39a9d549cd2cd70ba10d35805090cd8a8ee1caef
Pietropanzeri <pietro.panzeri@issgreppi.it>
2023-11-21 17:01:50 +0100

    1.2.0 Lista novità home page

--- Client/View/HomePage.xaml.cs
+++ Client/View/HomePage.xaml.cs
@@ -1,9 +1,13 @@
+using Client.Controller;
+using System.Runtime.CompilerServices;
+
 namespace Client.View;
 
 public partial class HomePage : ContentPage
 {
 	public HomePage()
 	{
+		BindingContext = new HomePageController();
 		InitializeComponent();
 	}
 }
