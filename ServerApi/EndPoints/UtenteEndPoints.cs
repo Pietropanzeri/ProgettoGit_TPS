2e4bb643416f8e5d8f5910482b73ce612cb3fdec
Pietropanzeri <pietro.panzeri@issgreppi.it>
2023-11-23 20:04:54 +0100

    3.6.0 Api Login

--- ServerApi/EndPoints/UtenteEndPoints.cs
+++ ServerApi/EndPoints/UtenteEndPoints.cs
@@ -13,7 +13,7 @@ namespace ServerApi.EndPoints
         public static void MapUtenteEndPoints(IEndpointRouteBuilder endpoint)
         {
             endpoint.MapGet("/utente", async (RicettarioDbContext db) =>
-             Results.Ok(await db.Utenti.Select(u => new UtenteDTO(u)).ToListAsync()));
+                Results.Ok(await db.Utenti.Select(u => new UtenteDTO(u)).ToListAsync()));
 
             endpoint.MapGet("/utente/{utenteId}", async (RicettarioDbContext db, int utenteId) =>
                 await db.Utenti.FindAsync(utenteId)
@@ -30,30 +30,33 @@ namespace ServerApi.EndPoints
                     Password = HashPassword(utenteDto.Password),
                     FotoId = 0
                 };
-                List<Utente> utentiConNomeUguale = await db.Utenti.Where(u => u.Username == utenteDto.Username).ToListAsync();
+                List<Utente> utentiConNomeUguale =
+                    await db.Utenti.Where(u => u.Username == utenteDto.Username).ToListAsync();
                 if (!utentiConNomeUguale.IsNullOrEmpty())
                 {
                     return Results.Conflict("Utente già esistente");
                 }
-                
-                
+
+
                 await db.Utenti.AddAsync(utente);
                 await db.SaveChangesAsync();
+                utente.Password = "";
                 return Results.Created($"/utente/{utente.UtenteId}", new UtenteDTO(utente));
 
             });
 
-            endpoint.MapPut("/utente/{utenteId}", async (RicettarioDbContext db, UtenteDTO updateUtente, int utenteId) =>
-            {
-                var utente = await db.Utenti.FindAsync(utenteId);
-                if (utente is null) return Results.NotFound();
+            endpoint.MapPut("/utente/{utenteId}",
+                async (RicettarioDbContext db, UtenteDTO updateUtente, int utenteId) =>
+                {
+                    var utente = await db.Utenti.FindAsync(utenteId);
+                    if (utente is null) return Results.NotFound();
 
-                if (!updateUtente.Username.IsNullOrEmpty())
-                    utente.Username = updateUtente.Username;
+                    if (!updateUtente.Username.IsNullOrEmpty())
+                        utente.Username = updateUtente.Username;
 
-                await db.SaveChangesAsync();
-                return Results.NoContent();
-            });
+                    await db.SaveChangesAsync();
+                    return Results.NoContent();
+                });
 
             endpoint.MapDelete("utente/{utenteId}", async (RicettarioDbContext db, int utenteId) =>
             {
@@ -65,17 +68,37 @@ namespace ServerApi.EndPoints
                 await db.SaveChangesAsync();
                 return Results.Ok();
             });
-        }
-        private static string HashPassword(string password)
-        {
-            using (var sha256 = SHA256.Create())
+            endpoint.MapPost("login",async (RicettarioDbContext db, UtenteDTO utenteDto) =>
             {
-                // Assicurati di avere aggiunto la direttiva using System.Text;
-                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var utente = await db.Utenti.Where(u => u.Username == utenteDto.Username && u.Password == HashPassword(utenteDto.Password)).FirstOrDefaultAsync();
+                if (utente is null)
+                    return Results.Unauthorized();
+                //TODO: Gestire JWT
+                utente.Password = "";
+                return Results.Ok(new UtenteDTO(utente));
+            });
+        }
 
-                // Converte l'hash in una stringa esadecimale
-                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+        private static string HashPassword(string clearText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
             }
+            return clearText;
         }
     }
 }
